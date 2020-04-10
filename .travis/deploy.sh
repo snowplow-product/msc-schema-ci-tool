7efe42e2b29a03160a6bafa8d3207fc2b28dbdd8
Andrea Marcolin <andrea@snowplowanalytics.com>
2020-04-10 14:08:27 +0200

    Add debug statement and make deploy script executable

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,9 @@ tag=$1
 
 cd "${TRAVIS_BUILD_DIR}"
 export TRAVIS_BUILD_RELEASE_TAG="${tag}"
+
+echo "DEPLOY: starting schema-ci deploy for version $TRAVIS_BUILD_RELEASE_TAG..."
+
 release-manager \
     --config "./.travis/release.yml" \
     --check-version \
